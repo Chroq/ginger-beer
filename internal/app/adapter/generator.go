12f6832739aef5877be3909e863aa48d3dd3aa4f
ChristopheLecroq <christophe.lecroq@limengo.com>
2022-11-23 10:46:46 +0100

    feat(generator): initialize generator adapter

---
+++ internal/app/adapter/generator.go
@@ -0,0 +1,16 @@
+package adapter
+
+// Generator is the entry point for the application
+type Generator struct {
+}
+
+// NewGenerator creates a new instance of the Generator
+func NewGenerator() *Generator {
+	return &Generator{}
+}
+
+// Build executes the application
+func (g *Generator) Build() error {
+
+	return nil
+}
