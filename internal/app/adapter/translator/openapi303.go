4015707e608fcd8f580129d061e55c38402a690b
ChristopheLecroq <christophe.lecroq@limengo.com>
2022-11-29 09:55:14 +0100

    feat(translator): add a translator from PostgreSQL data type to OpenAPI 3.0.3

---
+++ internal/app/adapter/translator/openapi303.go
@@ -0,0 +1,177 @@
+package translator
+
+import "fmt"
+
+// https://www.postgresql.org/docs/current/datatype.html
+const (
+	OpenAPITypeInteger      = "integer"
+	OpenAPITypeString       = "string"
+	OpenAPITypeBoolean      = "boolean"
+	OpenAPITypeNumber       = "number"
+	SQLTypeBigint           = "bigint"
+	SQLTypeInt8             = "int8"
+	SQLTypeBigSerial        = "bigserial"
+	SQLTypeSerial8          = "serial8"
+	SQLTypeBit              = "bit"
+	SQLTypeBitVarying       = "bit varying"
+	SQLTypeVarBit           = "varbit"
+	SQLTypeBool             = "bool"
+	SQLTypeBoolean          = "boolean"
+	SQLTypeBox              = "box"
+	SQLTypeBytea            = "bytea"
+	SQLTypeCharacter        = "character"
+	SQLTypeCharacterVarying = "character varying"
+	SQLTypeVarchar          = "varchar"
+	SQLTypeCidr             = "cidr"
+	SQLTypeCircle           = "circle"
+	SQLTypeDate             = "date"
+	SQLTypeDoublePrecision  = "double precision"
+	SQLTypeInet             = "inet"
+	SQLTypeInteger          = "integer"
+	SQLTypeInt              = "int"
+	SQLTypeInt4             = "int4"
+	SQLTypeInterval         = "interval"
+	SQLTypeJson             = "json"
+	SQLTypeJsonb            = "jsonb"
+	SQLTypeLine             = "line"
+	SQLTypeLSEG             = "lseg"
+	SQLTypeMACAddr          = "macaddr"
+	SQLTypeMACAddr8         = "macaddr8"
+	SQLTypeMoney            = "money"
+	SQLTypeNumeric          = "numeric"
+	SQLTypeDecimal          = "decimal"
+	SQLTypePath             = "path"
+	SQLTypePgLsn            = "pg_lsn"
+	SQLTypePgSnapshot       = "pg_snapshot"
+	SQLTypePoint            = "point"
+	SQLTypePolygon          = "polygon"
+	SQLTypeReal             = "real"
+	SQLTypeFloat4           = "float4"
+	SQLTypeSmallint         = "smallint"
+	SQLTypeInt2             = "int2"
+	SQLTypeSmallSerial      = "smallserial"
+	SQLTypeSerial2          = "serial2"
+	SQLTypeSerial           = "serial"
+	SQLTypeSerial4          = "serial4"
+	SQLTypeText             = "text"
+	SQLTypeTime             = "time"
+	SQLTypeTimestamp        = "timestamp"
+	SQLTypeTsQuery          = "tsquery"
+	SQLTypeTsVector         = "tsvector"
+	SQLTypeTxIDSnapshot     = "txid_snapshot"
+	SQLTypeUuid             = "uuid"
+	SQLTypeXml              = "xml"
+)
+
+func SQLToOpenAPIScalarTypes(t string) (string, error) {
+	switch t {
+	case SQLTypeBigint:
+		return OpenAPITypeInteger, nil
+	case SQLTypeInt8:
+		return OpenAPITypeInteger, nil
+	case SQLTypeBigSerial:
+		return OpenAPITypeInteger, nil
+	case SQLTypeSerial8:
+		return OpenAPITypeInteger, nil
+	case SQLTypeBit:
+		return OpenAPITypeString, nil
+	case SQLTypeBitVarying:
+		return OpenAPITypeString, nil
+	case SQLTypeVarBit:
+		return OpenAPITypeString, nil
+	case SQLTypeBool:
+		return OpenAPITypeBoolean, nil
+	case SQLTypeBoolean:
+		return OpenAPITypeBoolean, nil
+	case SQLTypeBox:
+		return OpenAPITypeString, nil
+	case SQLTypeBytea:
+		return OpenAPITypeString, nil
+	case SQLTypeCharacter:
+		return OpenAPITypeString, nil
+	case SQLTypeCharacterVarying:
+		return OpenAPITypeString, nil
+	case SQLTypeVarchar:
+		return OpenAPITypeString, nil
+	case SQLTypeCidr:
+		return OpenAPITypeString, nil
+	case SQLTypeCircle:
+		return OpenAPITypeString, nil
+	case SQLTypeDate:
+		return OpenAPITypeString, nil
+	case SQLTypeDoublePrecision:
+		return OpenAPITypeNumber, nil
+	case SQLTypeInet:
+		return OpenAPITypeString, nil
+	case SQLTypeInteger:
+		return OpenAPITypeInteger, nil
+	case SQLTypeInt:
+		return OpenAPITypeInteger, nil
+	case SQLTypeInt4:
+		return OpenAPITypeInteger, nil
+	case SQLTypeInterval:
+		return OpenAPITypeString, nil
+	case SQLTypeJson:
+		return OpenAPITypeString, nil
+	case SQLTypeJsonb:
+		return OpenAPITypeString, nil
+	case SQLTypeLine:
+		return OpenAPITypeString, nil
+	case SQLTypeLSEG:
+		return OpenAPITypeString, nil
+	case SQLTypeMACAddr:
+		return OpenAPITypeString, nil
+	case SQLTypeMACAddr8:
+		return OpenAPITypeString, nil
+	case SQLTypeMoney:
+		return OpenAPITypeNumber, nil
+	case SQLTypeNumeric:
+		return OpenAPITypeNumber, nil
+	case SQLTypeDecimal:
+		return OpenAPITypeNumber, nil
+	case SQLTypePath:
+		return OpenAPITypeString, nil
+	case SQLTypePgLsn:
+		return OpenAPITypeString, nil
+	case SQLTypePgSnapshot:
+		return OpenAPITypeString, nil
+	case SQLTypePoint:
+		return OpenAPITypeString, nil
+	case SQLTypePolygon:
+		return OpenAPITypeString, nil
+	case SQLTypeReal:
+		return OpenAPITypeNumber, nil
+	case SQLTypeFloat4:
+		return OpenAPITypeNumber, nil
+	case SQLTypeSmallint:
+		return OpenAPITypeInteger, nil
+	case SQLTypeInt2:
+		return OpenAPITypeInteger, nil
+	case SQLTypeSmallSerial:
+		return OpenAPITypeInteger, nil
+	case SQLTypeSerial2:
+		return OpenAPITypeInteger, nil
+	case SQLTypeSerial:
+		return OpenAPITypeInteger, nil
+	case SQLTypeSerial4:
+		return OpenAPITypeInteger, nil
+	case SQLTypeText:
+		return OpenAPITypeString, nil
+	case SQLTypeTime:
+		return OpenAPITypeString, nil
+	case SQLTypeTimestamp:
+		return OpenAPITypeString, nil
+	case SQLTypeTsQuery:
+		return OpenAPITypeString, nil
+	case SQLTypeTsVector:
+		return OpenAPITypeString, nil
+	case SQLTypeTxIDSnapshot:
+		return OpenAPITypeString, nil
+	case SQLTypeUuid:
+		return OpenAPITypeString, nil
+	case SQLTypeXml:
+		return OpenAPITypeString, nil
+	default:
+		return "", fmt.Errorf("invalid SQL type: %s", t)
+	}
+}
---
+++ internal/app/adapter/translator/openapi303_test.go
@@ -0,0 +1,354 @@
+package translator
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/maxatome/go-testdeep/td"
+)
+
+func TestSQLToOpenAPIScalarTypes(t *testing.T) {
+	type args struct {
+		sqlType string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+		err  error
+	}{
+		{
+			name: "bigint should return integer",
+			args: args{
+				sqlType: "bigint",
+			},
+			want: "integer",
+		}, {
+			name: "int8 should return integer",
+			args: args{
+				sqlType: "int8",
+			},
+			want: "integer",
+		}, {
+			name: "bigserial should return integer",
+			args: args{
+				sqlType: "bigserial",
+			},
+			want: "integer",
+		}, {
+			name: "serial8 should return integer",
+			args: args{
+				sqlType: "serial8",
+			},
+			want: "integer",
+		}, {
+			name: "bit should return string",
+			args: args{
+				sqlType: "bit",
+			},
+			want: "string",
+		}, {
+			name: "bit varying should return string",
+			args: args{
+				sqlType: "bit varying",
+			},
+			want: "string",
+		}, {
+			name: "varbit should return string",
+			args: args{
+				sqlType: "varbit",
+			},
+			want: "string",
+		}, {
+			name: "bool should return boolean",
+			args: args{
+				sqlType: "bool",
+			},
+			want: "boolean",
+		}, {
+			name: "boolean should return boolean",
+			args: args{
+				sqlType: "boolean",
+			},
+			want: "boolean",
+		}, {
+			name: "box should return string",
+			args: args{
+				sqlType: "box",
+			},
+			want: "string",
+		}, {
+			name: "bytea should return string",
+			args: args{
+				sqlType: "bytea",
+			},
+			want: "string",
+		}, {
+			name: "character should return string",
+			args: args{
+				sqlType: "character",
+			},
+			want: "string",
+		}, {
+			name: "character varying should return string",
+			args: args{
+				sqlType: "character varying",
+			},
+			want: "string",
+		}, {
+			name: "varchar should return string",
+			args: args{
+				sqlType: "varchar",
+			},
+			want: "string",
+		}, {
+			name: "cidr should return string",
+			args: args{
+				sqlType: "cidr",
+			},
+			want: "string",
+		}, {
+			name: "circle should return string",
+			args: args{
+				sqlType: "circle",
+			},
+			want: "string",
+		}, {
+			name: "date should return string",
+			args: args{
+				sqlType: "date",
+			},
+			want: "string",
+		}, {
+			name: "double precision should return number",
+			args: args{
+				sqlType: "double precision",
+			},
+			want: "number",
+		}, {
+			name: "inet should return string",
+			args: args{
+				sqlType: "inet",
+			},
+			want: "string",
+		}, {
+			name: "integer should return integer",
+			args: args{
+				sqlType: "integer",
+			},
+			want: "integer",
+		}, {
+			name: "int should return integer",
+			args: args{
+				sqlType: "int",
+			},
+			want: "integer",
+		}, {
+			name: "int4 should return integer",
+			args: args{
+				sqlType: "int4",
+			},
+			want: "integer",
+		}, {
+			name: "interval should return string",
+			args: args{
+				sqlType: "interval",
+			},
+			want: "string",
+		}, {
+			name: "json	should return string",
+			args: args{
+				sqlType: "json",
+			},
+			want: "string",
+		}, {
+			name: "jsonb should return string",
+			args: args{
+				sqlType: "jsonb",
+			},
+			want: "string",
+		}, {
+			name: "line should return string",
+			args: args{
+				sqlType: "line",
+			},
+			want: "string",
+		}, {
+			name: "lseg should return string",
+			args: args{
+				sqlType: "lseg",
+			},
+			want: "string",
+		}, {
+			name: "macaddr should return string",
+			args: args{
+				sqlType: "macaddr",
+			},
+			want: "string",
+		}, {
+			name: "macaddr8 should return string",
+			args: args{
+				sqlType: "macaddr8",
+			},
+			want: "string",
+		}, {
+			name: "money should return number",
+			args: args{
+				sqlType: "money",
+			},
+			want: "number",
+		}, {
+			name: "numeric should return number",
+			args: args{
+				sqlType: "numeric",
+			},
+			want: "number",
+		}, {
+			name: "decimal should return number",
+			args: args{
+				sqlType: "decimal",
+			},
+			want: "number",
+		}, {
+			name: "path should return string",
+			args: args{
+				sqlType: "path",
+			},
+			want: "string",
+		}, {
+			name: "pg_lsn should return string",
+			args: args{
+				sqlType: "pg_lsn",
+			},
+			want: "string",
+		}, {
+			name: "pg_snapshot should return string",
+			args: args{
+				sqlType: "pg_snapshot",
+			},
+			want: "string",
+		}, {
+			name: "point should return string",
+			args: args{
+				sqlType: "point",
+			},
+			want: "string",
+		}, {
+			name: "polygon should return string",
+			args: args{
+				sqlType: "polygon",
+			},
+			want: "string",
+		}, {
+			name: "real should return number",
+			args: args{
+				sqlType: "real",
+			},
+			want: "number",
+		}, {
+			name: "float4 should return number",
+			args: args{
+				sqlType: "float4",
+			},
+			want: "number",
+		}, {
+			name: "smallint should return integer",
+			args: args{
+				sqlType: "smallint",
+			},
+			want: "integer",
+		}, {
+			name: "int2 should return integer",
+			args: args{
+				sqlType: "int2",
+			},
+			want: "integer",
+		}, {
+			name: "smallserial should return integer",
+			args: args{
+				sqlType: "smallserial",
+			},
+			want: "integer",
+		}, {
+			name: "serial2 should return integer",
+			args: args{
+				sqlType: "serial2",
+			},
+			want: "integer",
+		}, {
+			name: "serial should return integer",
+			args: args{
+				sqlType: "serial",
+			},
+			want: "integer",
+		}, {
+			name: "serial4 should return integer",
+			args: args{
+				sqlType: "serial4",
+			},
+			want: "integer",
+		}, {
+			name: "text should return string",
+			args: args{
+				sqlType: "text",
+			},
+			want: "string",
+		}, {
+			name: "time should return string",
+			args: args{
+				sqlType: "time",
+			},
+			want: "string",
+		}, {
+			name: "timestamp should return string",
+			args: args{
+				sqlType: "timestamp",
+			},
+			want: "string",
+		}, {
+			name: "tsquery should return string",
+			args: args{
+				sqlType: "tsquery",
+			},
+			want: "string",
+		}, {
+			name: "tsvector should return string",
+			args: args{
+				sqlType: "tsvector",
+			},
+			want: "string",
+		}, {
+			name: "txid_snapshot should return string",
+			args: args{
+				sqlType: "txid_snapshot",
+			},
+			want: "string",
+		}, {
+			name: "uuid should return string",
+			args: args{
+				sqlType: "uuid",
+			},
+			want: "string",
+		}, {
+			name: "xml should return string",
+			args: args{
+				sqlType: "xml",
+			},
+			want: "string",
+		}, {
+			name: "invalid type should be detected",
+			args: args{
+				sqlType: "test",
+			},
+			want: "",
+			err:  errors.New("invalid SQL type: test"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			openAPIType, err := SQLToOpenAPIScalarTypes(tt.args.sqlType)
+			td.Cmp(t, err, tt.err)
+			td.Cmp(t, openAPIType, tt.want)
+		})
+	}
+}
