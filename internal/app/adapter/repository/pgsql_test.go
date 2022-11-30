2d10c5bf3481331142b35f02ad67d623721e8145
ChristopheLecroq <christophe.lecroq@limengo.com>
2022-11-30 18:46:34 +0100

    tests(repository): adapt test to the new repository function

--- internal/app/adapter/repository/pgsql_test.go
+++ internal/app/adapter/repository/pgsql_test.go
@@ -3,73 +3,13 @@ package repository_test
 import (
 	"database/sql"
 	"ginger-beer/internal/app/adapter/repository"
-	"ginger-beer/internal/app/domain"
+	"ginger-beer/internal/app/domain/valueobject"
 	"ginger-beer/testdata/tu"
 	"testing"
 
 	_ "github.com/lib/pq"
 	"github.com/maxatome/go-testdeep/td"
 )
-
-func TestSqlRepository_GetComponents(t *testing.T) {
-	type fields struct {
-		DB func() *sql.DB
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   *domain.Component
-		err    error
-		clean  func()
-	}{
-		{
-			name: "default",
-			fields: fields{
-				DB: func() *sql.DB {
-					db, err := sql.Open("postgres", "postgresql://postgres@localhost/ginger-beer-test?sslmode=disable")
-					td.CmpNoError(t, err)
-
-					_, err = db.Exec(`CREATE TABLE IF NOT EXISTS "test"(id SERIAL PRIMARY KEY, name VARCHAR(255));`)
-					td.CmpNoError(t, err)
-
-					return db
-				},
-			},
-			want: &domain.Component{
-				Schema: map[string]domain.Schema{
-					"Test": {
-						Type:       "object",
-						Properties: map[string]domain.Property{"id": {Type: "integer"}, "name": {Type: "string", MaxLength: tu.Ptr(255)}},
-					},
-				},
-			},
-			clean: func() {
-				db, err := sql.Open("postgres", "postgresql://postgres@localhost/ginger-beer-test?sslmode=disable")
-				defer func(db *sql.DB) {
-					err := db.Close()
-					td.CmpNoError(t, err)
-				}(db)
-				td.CmpNoError(t, err)
-
-				_, err = db.Exec(`DROP TABLE IF EXISTS "test";`)
-				td.CmpNoError(t, err)
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			r := &repository.SQLRepository{
-				DB: tt.fields.DB(),
-			}
-			defer r.DB.Close()
-			defer tt.clean()
-
-			got, err := r.GetFields()
-			td.Cmp(t, err, tt.err)
-			td.Cmp(t, got, tt.want)
-		})
-	}
-}
 
 func TestSQLRepository_GetEntities(t *testing.T) {
 	type fields struct {
@@ -78,7 +18,7 @@ func TestSQLRepository_GetEntities(t *testing.T) {
 	tests := []struct {
 		name   string
 		fields fields
-		want   []string
+		want   map[string][]*valueobject.Field
 		error  error
 		clean  func()
 	}{
@@ -95,7 +35,19 @@ func TestSQLRepository_GetEntities(t *testing.T) {
 					return db
 				},
 			},
-			want: []string{"test"},
+			want: map[string][]*valueobject.Field{
+				"test": {
+					{
+						Name: "id",
+						Type: "integer",
+					},
+					{
+						Name:      "name",
+						Type:      "string",
+						MaxLength: tu.Ptr(255),
+					},
+				},
+			},
 			clean: func() {
 				db, err := sql.Open("postgres", "postgresql://postgres@localhost/ginger-beer-test?sslmode=disable")
 				defer func(db *sql.DB) {
